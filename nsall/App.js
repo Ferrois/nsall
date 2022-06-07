766806c5fd76051964f9c4d9422ec2696099f422
Ferrois <ferrois2005@gmail.com>
2022-06-08 02:26:45 +0800

    Implemented Stack & Tab navigators, basic template for LoginPage.js added

--- nsall/App.js
+++ nsall/App.js
@@ -1,17 +1,17 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import useFetch from "./Hooks/useFetch.js";
 import useTimeLeft from "./Hooks/useTimeLeft.js";
 import { NativeBaseProvider } from "native-base";
+import LoginPage from "./Components/Pages/LoginPage.js";
 
 const Tab = createBottomTabNavigator();
-// const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 function Home() {
-  // const [data] = useFetch("https://sandbox.api.myinfo.gov.sg/com/v3/person-sample/S9812381D")
   const timeLeft = useTimeLeft("December 17, 2022 03:24:00");
   return (
     <View style={styles.container}>
@@ -21,25 +21,31 @@ function Home() {
         {timeLeft[0]}days, {timeLeft[1]}hours, {timeLeft[2]}minutes,{" "}
         {timeLeft[3]}seconds
       </Text>
-      {/* <Text>{timeLeft}</Text> */}
-      {/* <Text>{data && data.map((obj)=>{return(<Text key={obj.id}>id:{obj.id}  </Text>)})}</Text> */}
-      {/* <Text>{JSON.stringify(data)}</Text> */}
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function HomeInterface() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home}></Tab.Screen>
+        <Tab.Screen name="Chao geng" component={Home}></Tab.Screen>
+        <Tab.Screen name="Utilities" component={Home}></Tab.Screen>
+        <Tab.Screen name="Settings" component={Home}></Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 export default function App() {
-  // const [data] = useFetch("https://jsonplaceholder.typicode.com/todos")
   return (
     <NativeBaseProvider>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home}></Tab.Screen>
-          <Tab.Screen name="Chao geng" component={Home}></Tab.Screen>
-          <Tab.Screen name="Utilities" component={Home}></Tab.Screen>
-          <Tab.Screen name="Settings" component={Home}></Tab.Screen>
-        </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={LoginPage} />
+        <Stack.Screen name="Interface" component={HomeInterface} />
+      </Stack.Navigator>
       </NavigationContainer>
     </NativeBaseProvider>
   );
---
+++ nsall/Components/Pages/LoginPage.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <View>
+        <Text>Login page</Text>
+    </View>
+  )
+}
