8e85479093c6764e71010b6da2180b89c32985b9
Ferrois <ferrois2005@gmail.com>
2022-06-07 23:48:07 +0800

    Added react navigation tab

--- nsall/App.js
+++ nsall/App.js
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import useFetch from './Hooks/useFetch.js'
 import useTimeLeft from './Hooks/useTimeLeft.js';
 
 export default function App() {
-  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos")
+  // const [data] = useFetch("https://jsonplaceholder.typicode.com/todos")
+  const [data] = useFetch("https://sandbox.api.myinfo.gov.sg/com/v3/person-sample/S9812381D")
   const timeLeft = useTimeLeft("December 17, 2022 03:24:00");
   return (
     <View style={styles.container}>
       <Text>our brand new ns app - ferrois</Text>
       <Text>Target date: December 17, 2022 03:24:00</Text>
       <Text>{timeLeft[0]}days, {timeLeft[1]}hours, {timeLeft[2]}minutes, {timeLeft[3]}seconds</Text>
-      <Text>{data && data.map((obj)=>{return(<Text key={obj.id}>id:{obj.id}  </Text>)})}</Text>
+      {/* <Text>{timeLeft}</Text> */}
+      {/* <Text>{data && data.map((obj)=>{return(<Text key={obj.id}>id:{obj.id}  </Text>)})}</Text> */}
+      {/* <Text>{JSON.stringify(data)}</Text> */}
       <StatusBar style="auto" />
     </View>
+    // <NavigationContainer></NavigationContainer>
   );
 }
 
