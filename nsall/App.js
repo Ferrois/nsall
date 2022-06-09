7947417daa30f2bb3a859434fb6b8fdc7199ddd4
AngelLXY <162133128qq@gmail.com>
2022-06-09 16:06:30 +0800

    Updated login page

--- nsall/App.js
+++ nsall/App.js
@@ -2,18 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import useFetch from './Hooks/useFetch.js'
 import useTimeLeft from './Hooks/useTimeLeft.js';
+import LoginPage from './Components/Pages/LoginPage.js';
 
 export default function App() {
   const [data] = useFetch("https://jsonplaceholder.typicode.com/todos")
   const timeLeft = useTimeLeft("December 17, 2022 03:24:00");
   return (
-    <View style={styles.container}>
-      <Text>our brand new ns app - ferrois</Text>
-      <Text>Target date: December 17, 2022 03:24:00</Text>
-      <Text>{timeLeft[0]}days, {timeLeft[1]}hours, {timeLeft[2]}minutes, {timeLeft[3]}seconds</Text>
-      <Text>{data && data.map((obj)=>{return(<Text key={obj.id}>id:{obj.id}  </Text>)})}</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>our brand new ns app - ferrois</Text>
+    //   <Text>Target date: December 17, 2022 03:24:00</Text>
+    //   <Text>{timeLeft[0]}days, {timeLeft[1]}hours, {timeLeft[2]}minutes, {timeLeft[3]}seconds</Text>
+    //   <Text>{data && data.map((obj)=>{return(<Text key={obj.id}>id:{obj.id}  </Text>)})}</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <LoginPage>
+      
+    </LoginPage>
   );
 }
 
---
+++ nsall/Components/Pages/LoginPage.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function LoginPage() {
+  const [show, setShow] = React.useState(false);
+  return (
+    <Stack space={4} w="100%" alignItems="center">
+      <Input
+        w={{
+          base: "75%",
+          md: "25%",
+        }}
+        InputLeftElement={
+          <Icon
+            as={<MaterialIcons name="person" />}
+            size={5}
+            ml="2"
+            color="muted.400"
+          />
+        }
+        placeholder="Name"
+      />
+      <Input
+        w={{
+          base: "75%",
+          md: "25%",
+        }}
+        type={show ? "text" : "password"}
+        InputRightElement={
+          <Icon
+            as={<MaterialIcons name={show ? "visibility" : "visibility-off"} />}
+            size={5}
+            mr="2"
+            color="muted.400"
+            onPress={() => setShow(!show)}
+          />
+        }
+        placeholder="Password"
+      />
+    </Stack>
+  );
+}
