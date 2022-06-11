949129df6969e40b522cdc227890396a6f3d0b65
AngelLXY <162133128qq@gmail.com>
2022-06-11 17:50:11 +0800

    setting up map page 2nd

--- nsall/Components/Pages/NSafePage.js
+++ nsall/Components/Pages/NSafePage.js
@@ -1,24 +1,35 @@
-import { Box, Center, Text } from "native-base";
+import { Box, Center, Text, View, StyleSheet } from "native-base";
 import React from "react";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export default function NSafePage() {
   return (
-    <Box bg="primary.400" p="12" rounded="lg">
-      <Center>
-        <Text>NSafe tracking location, pair with watch</Text>
-        <MapView
-          style={{ flex: 1 }}
-          provider={PROVIDER_GOOGLE}
-          showsUserLocation
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </Center>
-    </Box>
+    // <Box bg="primary.400" p="12" rounded="lg">
+    //   <Center>
+    //     <Text>NSafe tracking location, pair with watch</Text>
+    //     <MapView
+    //       style={{ flex: 1 }}
+    //       provider={PROVIDER_GOOGLE}
+    //       showsUserLocation
+    //       initialRegion={{
+    //         latitude: 37.78825,
+    //         longitude: -122.4324,
+    //         latitudeDelta: 0.0922,
+    //         longitudeDelta: 0.0421,
+    //       }}
+    //     />
+    //   </Center>
+    // </Box>
+    <View style={StyleSheet.body}>
+
+    </View>
   );
 }
+
+const style = StyleSheet.create({
+  body :{
+    flex: 1,
+    alighItems: "center",
+
+  },
+})
