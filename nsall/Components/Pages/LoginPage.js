38831c9ff9740ee1e662636fbcb4c3b3bbdd094d
AngelLXY <162133128qq@gmail.com>
2022-06-12 01:27:18 +0800

    login page w/o backend

--- nsall/Components/Pages/LoginPage.js
+++ nsall/Components/Pages/LoginPage.js
@@ -1,9 +1,9 @@
-import { Image, Button, Box, Input } from "native-base";
-import { StyleSheet, useWindowDimensions } from "react-native";
+import { Image, Button, Box, Input, Text, ScrollView } from "native-base";
+import { StyleSheet, useWindowDimensions, Pressable } from "react-native";
 import React, { useState } from "react";
 import Logo from "../../assets/NSALLlogo.png";
 
-const LoginPage = ({ navigation }) => {
+const LoginPage = ({ navigation, onPress }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { height } = useWindowDimensions();
@@ -14,34 +14,57 @@ const LoginPage = ({ navigation }) => {
     });
   };
   return (
-    <Box style={styles.root}>
-      <Image
-        source={Logo}
-        style={[styles.logo, { height: height * 0.2 }]}
-        resizeMode="contain"
-        alt="logo"
-      />
-      <Input
-        placeholder="Username"
-        value={username}
-        setValue={setUsername}
-        mt={2}
-      />
-      <Input
-        placeholder="Password"
-        value={password}
-        setValue={setPassword}
-        mt={2}
-      />
-      <Button
-        onPress={() => {
-          handleLogin();
-        }}
-        mt={2}
-      >
-        Navigate into app
-      </Button>
-    </Box>
+    <ScrollView showsVerticalScrollIndicator={false}> 
+      <Box style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, { height: height * 0.2 }]}
+          resizeMode="contain"
+          alt="logo"
+        />
+        <Input
+          placeholder="Username"
+          value={username}
+          setValue={setUsername}
+          mt={2}
+        />
+        <Input
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry={true}
+          mt={2}
+        />
+        <Button onPress={onPress} style={styles.button1}>
+          <Text style={styles.text1}>Log In</Text>
+        </Button>
+
+        <Button style={styles.button2} onPress={onPress}>
+          <Text style={styles.text}>Forget password?</Text>
+        </Button>
+
+        <Button style={styles.button3} onPress={onPress}>
+          <Text style={styles.text3}>Sign in with google</Text>
+        </Button>
+
+        <Button style={styles.button4} onPress={onPress}>
+          <Text style={styles.text4}>Sign in with facebook</Text>
+        </Button>
+
+        <Button style={styles.button2} onPress={onPress}>
+          <Text style={styles.text5}>Create an account</Text>
+        </Button>
+
+        <Button
+          onPress={() => {
+            handleLogin();
+          }}
+          mt={2}
+        >
+          Navigate into app
+        </Button>
+      </Box>
+    </ScrollView>
   );
 };
 
@@ -55,6 +78,55 @@ const styles = StyleSheet.create({
   logo: {
     width: "80%",
     borderRadius: 10,
+  },
+  button1: {
+    backgroundColor: "#3B71F3",
+    width: "60%",
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 10,
+  },
+  button2: {
+    backgroundColor: "#F9FBFC",
+    width: "60%",
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 10,
+  },
+  button3: {
+    backgroundColor: "#FAE9EA",
+    width: "60%",
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 10,
+  },
+  button4: {
+    backgroundColor: "#E7EAF4",
+    width: "60%",
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 10,
+  },
+
+  text1: {
+    fontWeight: "bold",
+    color: "white",
+  },
+  text2: {
+    fontWeight: "bold",
+    color: "grey",
+  },
+  text3: {
+    fontWeight: "bold",
+    color: "#DD4D44",
+  },
+  text4: {
+    fontWeight: "bold",
+    color: "#4765A9",
+  },
+  text5: {
+    fontWeight: "bold",
+    color: "black",
   },
 });
 export default LoginPage;
