976f5cfbfa3c54e14fcd31375e579600c69d2d15
Ferrois <ferrois2005@gmail.com>
2022-06-12 00:35:37 +0800

    Updated LoginPage.js

--- nsall/Components/Pages/LoginPage.js
+++ nsall/Components/Pages/LoginPage.js
@@ -1,51 +1,60 @@
-import { Text, View, Image, Button } from "native-base";
+import { Image, Button, Box, Input } from "native-base";
 import { StyleSheet, useWindowDimensions } from "react-native";
 import React, { useState } from "react";
 import Logo from "../../assets/NSALLlogo.png";
-import CustomInput from "./CustomInput";
 
-const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-const LoginPage = ({navigation}) => {
-  const {height} = useWindowDimensions();
+const LoginPage = ({ navigation }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { height } = useWindowDimensions();
   const handleLogin = () => {
     navigation.reset({
-      index:0,
-      routes: [{ name: 'Interface' }]
-    })
-  }
+      index: 0,
+      routes: [{ name: "Interface" }],
+    });
+  };
   return (
     <Box style={styles.root}>
       <Image
         source={Logo}
-        style={[styles.logo,  { height: height * 0.2 }]}
+        style={[styles.logo, { height: height * 0.2 }]}
         resizeMode="contain"
         alt="logo"
       />
-      <CustomInput placeholder="Username" value={username} setValue={setUsername}/>
-
-      <CustomInput placeholder="Password" value={password} setValue={setPassword}/>
-      <Button onPress={()=>{handleLogin()}}>Navigate into app</Button>
+      <Input
+        placeholder="Username"
+        value={username}
+        setValue={setUsername}
+        mt={2}
+      />
+      <Input
+        placeholder="Password"
+        value={password}
+        setValue={setPassword}
+        mt={2}
+      />
+      <Button
+        onPress={() => {
+          handleLogin();
+        }}
+        mt={2}
+      >
+        Navigate into app
+      </Button>
     </Box>
-      
-    
   );
 };
 
-
-
 const styles = StyleSheet.create({
   root: {
-    flex : 1,
+    flex: 1,
     backgroundColor: "#F9FBFC",
     alignItems: "center",
     padding: 20,
   },
   logo: {
     width: "80%",
-    borderRadius: 10
+    borderRadius: 10,
   },
 });
-}
 export default LoginPage;
