e8686b1d1cbf81f1b2163a7fce5fa96233707292
Ferrois <ferrois2005@gmail.com>
2022-06-11 22:36:25 +0800

    Socket.io server initialised

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = 4000;
+const { Server } = require("socket.io");
+const { v4 } = require("uuid");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection",(socket)=>{
+    console.log(socket.id)
+    socket.on("hello",()=>{
+        console.log('received message')
+    })
+})
+
+server.listen(PORT,()=>{
+    console.log("testing on port",PORT)
+})
+// require("dotenv").config();
