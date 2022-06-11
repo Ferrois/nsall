cf20f1d6f1a74b57dd824832f89acf0085c827b8
Ferrois <ferrois2005@gmail.com>
2022-06-11 15:30:52 +0800

    Added unfunctional Map component at NSafePage.js

--- nsall/Components/Pages/NSafePage.js
+++ nsall/Components/Pages/NSafePage.js
@@ -1,11 +1,23 @@
 import { Box, Center, Text } from "native-base";
 import React from "react";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
 export default function NSafePage() {
   return (
     <Box bg="primary.400" p="12" rounded="lg">
       <Center>
         <Text>NSafe tracking location, pair with watch</Text>
+        <MapView
+          style={{ flex: 1 }}
+          provider={PROVIDER_GOOGLE}
+          showsUserLocation
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
       </Center>
     </Box>
   );
---
+++ nsall/Components/Pages/UtilsPage.js
@@ -0,0 +1,10 @@
+import { Box } from "native-base";
+import React from "react";
+
+export default function UtilsPage() {
+  return (
+    <Box safeArea>
+      <Center>Utilities</Center>
+    </Box>
+  );
+}
