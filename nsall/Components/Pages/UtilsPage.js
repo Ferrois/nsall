5a00d8beaeb5e66bbbe116f2d7e53440c52479a3
Ferrois <ferrois2005@gmail.com>
2022-06-14 17:38:36 +0800

    More page navigator

--- nsall/Components/Pages/UtilsPage.js
+++ nsall/Components/Pages/UtilsPage.js
@@ -7,13 +7,16 @@ import { socket } from "../../Helpers/socket";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import MorePage from "./MoreComps/MorePage";
+import VideosPage from "./MoreComps/VideosPage";
+import EmartPage from "./MoreComps/EmartPage";
+import TipsPage from "./MoreComps/TipsPage";
 
 const Stack = createNativeStackNavigator();
 
 export default function UtilsPage() {
-  const { storeCtx, themeCtx} = useContext(StoreContext);
-  const [store,setStore] = storeCtx;
-  const [theme, setTheme]= themeCtx;
+  const { storeCtx, themeCtx } = useContext(StoreContext);
+  const [store, setStore] = storeCtx;
+  const [theme, setTheme] = themeCtx;
   return (
     // <>
     <NavigationContainer independent={true}>
@@ -21,6 +24,21 @@ export default function UtilsPage() {
         <Stack.Screen
           name="MorePage"
           component={MorePage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Video"
+          component={VideosPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Emart"
+          component={EmartPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Tips"
+          component={TipsPage}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
